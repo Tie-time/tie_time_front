7e4f2365a628ee87069e489a15ed3f2ea592e7af
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-10-11 21:50:54 +0200

    Revert "style(Signin): [Theme] Add global theme"
    This reverts commit 26be02d6586aade672f9cd474becc93da2d1b497.

--- lib/pages/main.page.dart
+++ lib/pages/main.page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:tie_time_front/routes/routes.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,28 +8,16 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  void _handleLogout() async {
-    // clear cache
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.remove('token');
-    Navigator.pushReplacementNamed(context, RouteManager.home);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text('Tie Time'),
-        ),
-        body: Center(
-            child: Column(children: [
-          Text('Welcome to Tie Time!'),
-          FilledButton(
-              onPressed: () {
-                _handleLogout();
-              },
-              child: Text('Se déconnecter')),
-        ])));
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Tie Time'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Tie Time!'),
+      ),
+    );
   }
 }
