1bd45e1ee118467d098384c914598edec0523242
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-10-11 21:53:39 +0200

    feat(Account): [Logout] Add logout

--- lib/pages/main.page.dart
+++ lib/pages/main.page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tie_time_front/routes/routes.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,16 +10,28 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  void _handleLogout() async {
+    // clear cache
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+    Navigator.pushReplacementNamed(context, RouteManager.home);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Tie Time'),
-      ),
-      body: const Center(
-        child: Text('Welcome to Tie Time!'),
-      ),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Tie Time'),
+        ),
+        body: Center(
+            child: Column(children: [
+          Text('Welcome to Tie Time!'),
+          FilledButton(
+              onPressed: () {
+                _handleLogout();
+              },
+              child: Text('Se déconnecter')),
+        ])));
   }
 }
