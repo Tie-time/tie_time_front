7162bb35306eea6fe6b2a3484ea1511cdabc6567
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2024-11-07 07:00:00 +0100

    feat(Task): Add get task from api gh-28

---
+++ lib/services/task.service.dart
@@ -0,0 +1,15 @@
+import 'package:tie_time_front/models/task.model.dart';
+import 'package:tie_time_front/services/api.service.dart';
+
+class TaskService {
+  final ApiService apiService;
+  final String prefix = '/tasks';
+
+  TaskService({required this.apiService});
+
+  Future<List<Task>> tasks(String currendDate) async {
+    List<dynamic> responseBody =
+        await apiService.get('$prefix/?date=$currendDate');
+    return responseBody.map((task) => Task.fromJson(task)).toList();
+  }
+}
--- lib/widgets/lists/tasks.list.dart
+++ lib/widgets/lists/tasks.list.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:tie_time_front/config/environnement.config.dart';
 import 'package:tie_time_front/models/task.model.dart';
+import 'package:tie_time_front/services/api.service.dart';
+import 'package:tie_time_front/services/messages.service.dart';
+import 'package:tie_time_front/services/task.service.dart';
 import 'package:tie_time_front/widgets/cards/task.card.dart';
 
 class TasksList extends StatefulWidget {
@@ -14,47 +19,32 @@ class TasksList extends StatefulWidget {
 class _TasksListState extends State<TasksList> {
   // fetch tasks by date
   // add tasks with date
-  late List<Task> _tasks;
+  final List<Task> _tasks = [];
+
+  late Future<List<Task>> _futureTasks;
+  late TaskService _taskService;
   late DateTime _currentDate;
 
   @override
   void initState() {
     super.initState();
-    _tasks = [
-      Task(
-        id: '1',
-        title: 'Tâche 1',
-        isChecked: false,
-        date: DateTime.now(),
-        order: 1,
-      ),
-      Task(
-        id: '2',
-        title: 'Tâche 2',
-        isChecked: false,
-        date: DateTime.now(),
-        order: 2,
-      ),
-      Task(
-        id: '3',
-        title: 'Tâche 3',
-        isChecked: false,
-        date: DateTime.now(),
-        order: 3,
-      ),
-    ];
     _currentDate = widget.currentDate;
+    _taskService =
+        TaskService(apiService: ApiService(baseUrl: Environnement.apiUrl));
+    _futureTasks = _taskService.tasks(_currentDate.toString());
   }
 
   void _addTask() {
     setState(() {
-      _tasks.add(Task(
-          id: UniqueKey().toString(),
-          title: '',
-          isChecked: false,
-          date: _currentDate,
-          order: _tasks.length,
-          isEditing: true));
+      _futureTasks.then((tasks) {
+        _tasks.add(Task(
+            id: UniqueKey().toString(),
+            title: '',
+            isChecked: false,
+            date: _currentDate,
+            order: _tasks.length,
+            isEditing: true));
+      });
     });
   }
 
@@ -67,16 +57,31 @@ class _TasksListState extends State<TasksList> {
           textAlign: TextAlign.start,
         ),
         const SizedBox(height: 16.0),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: _tasks
-              .map((task) => Column(
-                    children: [
-                      TaskCard(task: task),
-                      SizedBox(height: 16.0), // Espace entre les éléments
-                    ],
-                  ))
-              .toList(),
+        FutureBuilder<List<Task>>(
+          future: _futureTasks,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: _tasks
+                    .map((task) => Column(
+                          children: [
+                            TaskCard(task: task),
+                            SizedBox(height: 16.0), // Espace entre les éléments
+                          ],
+                        ))
+                    .toList(),
+              );
+            } else if (snapshot.hasError) {
+              SchedulerBinding.instance.addPostFrameCallback((_) {
+                MessageService.showErrorMessage(
+                    context, snapshot.error.toString());
+              });
+              return Container();
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
         ),
         FilledButton(
           onPressed: _addTask,
